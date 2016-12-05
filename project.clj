1400b6c3ae8ed8ee92d3883b9609ce04511012c1
Ben Sima <bensima@gmail.com>
2016-12-05 11:57:57 -0800

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.liaison/proletariat "0.2.0"
+(defproject com.liaison/proletariat "0.2.1"
   :description "Library of the Commons. A hard-working library of common utilities."
   :url "https://github.com/LiaisonTechnologies/proletariat"
 
