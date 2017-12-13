812d11fa8e27c666aafc5d9328bce9ba74fc64fe
Matt Anderson <manderson@liaison.com>
2017-12-13 13:38:48 -0500

    update versions

--- project.clj
+++ project.clj
@@ -6,15 +6,15 @@
   :profiles {:dev     {:plugins [[lein-ancient "0.6.10"]]}
              :uberjar {:source-paths ["src"]}}
 
-  :dependencies [[aero "1.1.1"]
-                 [aleph "0.4.3"]
+  :dependencies [[aero "1.1.2"]
+                 [aleph "0.4.4"]
                  [aysylu/loom "1.0.0" :exclusions [tailrecursion/cljs-priority-map]]
                  ;; This is a fix for a [bug](https://github.com/ztellman/aleph/issues/326)
                  ;; with nuisance error messages. When Aleph is upgraded to this version
                  ;; the explict dependency can be removed
                  [byte-streams "0.2.4-alpha3"]
                  [commons-codec/commons-codec "1.10"]
-                 [im.chit/hara.event "2.5.2"]
+                 [im.chit/hara.event "2.5.10"]
                  [org.clojure/clojure "1.9.0"]
                  [org.clojure/test.check "0.9.0"]
                  [org.clojure/tools.logging "0.4.0"]]
